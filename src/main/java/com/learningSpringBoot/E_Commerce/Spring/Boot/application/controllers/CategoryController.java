6dab6dd4db5f4b6b4ea4a848933dee367c942b9d
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2025-08-24 01:33:00 +0300

    feat: add CategoryController REST endpoints
    - Add GET /categories endpoint to list all categories
    - Add GET /categories/{id} endpoint to find one category
    - Add POST /categories endpoint to create a new category
    - Add PUT /categories/{id} endpoint to update a category
    - Controller uses DTOs for requests/responses and maps to/from entities

---
+++ src/main/java/com/learningSpringBoot/E_Commerce/Spring/Boot/application/controllers/CategoryController.java
@@ -0,0 +1,71 @@
+package com.learningSpringBoot.E_Commerce.Spring.Boot.application.controllers;
+
+
+import com.learningSpringBoot.E_Commerce.Spring.Boot.application.domain.dto.ApiResponseDto;
+import com.learningSpringBoot.E_Commerce.Spring.Boot.application.domain.dto.CategoryDto;
+import com.learningSpringBoot.E_Commerce.Spring.Boot.application.domain.entities.CategoryEntity;
+import com.learningSpringBoot.E_Commerce.Spring.Boot.application.mappers.Mapper;
+import com.learningSpringBoot.E_Commerce.Spring.Boot.application.services.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+    private final Mapper<CategoryEntity, CategoryDto> mapper;
+
+    public CategoryController(CategoryService categoryService, Mapper<CategoryEntity, CategoryDto> mapper) {
+        this.categoryService = categoryService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<ApiResponseDto<List<CategoryDto>>> findAllCategories() {
+        List<CategoryEntity> categories = categoryService.findAllCategories();
+        List<CategoryDto> listedCategoriesDto = categories.stream()
+                .map(mapper::mapTo)
+                .toList();
+
+        ApiResponseDto<List<CategoryDto>> response = new ApiResponseDto<>(
+                listedCategoriesDto.size(),  // recordCount
+                listedCategoriesDto          // response
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDto> findOneCategory(@PathVariable int id) {
+        CategoryEntity category = categoryService.findCategoryById(id);
+        CategoryDto existingCategoryDto = mapper.mapTo(category);
+
+        return new ResponseEntity<>(existingCategoryDto, HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<CategoryDto> createNewCategory(@RequestBody CategoryDto category) {
+        CategoryEntity categoryEntity = mapper.mapFrom(category);
+        // Call the service method for creation
+        CategoryDto savedCategoryDto = mapper.mapTo(categoryService.createCategory(categoryEntity));
+        return new ResponseEntity<>(savedCategoryDto, HttpStatus.CREATED);
+    }
+
+    // Add this new endpoint for updates
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryDto> updateExistingCategory(
+            @PathVariable("id") Integer id,
+            @RequestBody CategoryDto category) {
+
+        CategoryEntity categoryEntity = mapper.mapFrom(category);
+        // Call the  service method for update
+        CategoryDto updatedCategoryDto = mapper.mapTo(categoryService.updateCategory(id, categoryEntity));
+        return new ResponseEntity<>(updatedCategoryDto, HttpStatus.OK);
+    }
+}
