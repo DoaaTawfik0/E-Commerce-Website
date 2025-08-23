bf360c5e1211401be1482ac726266cba3ccbe073
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2025-08-23 16:26:39 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/learningSpringBoot/E_Commerce/Spring/Boot/application/ECommerceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.learningSpringBoot.E_Commerce.Spring.Boot.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningSpringBoot/E_Commerce/Spring/Boot/application/ECommerceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningSpringBoot.E_Commerce.Spring.Boot.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
