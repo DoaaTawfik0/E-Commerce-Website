ce65f86b8179c9b6e82fc0c576cf850dca55ca53
DoaaTawfik25 <doaa.25tawfik@gmail.com>
2025-08-24 20:24:55 +0300

    fix: correct CategoryMapperImpl implementation so it can correctly map from CategoryDto to CategoryEntity(map products)

--- src/main/java/com/learningSpringBoot/E_Commerce/Spring/Boot/application/mappers/impl/CategoryMapperImpl.java
+++ src/main/java/com/learningSpringBoot/E_Commerce/Spring/Boot/application/mappers/impl/CategoryMapperImpl.java
@@ -2,23 +2,37 @@ package com.learningSpringBoot.E_Commerce.Spring.Boot.application.mappers.impl;
 
 
 import com.learningSpringBoot.E_Commerce.Spring.Boot.application.domain.dto.CategoryDto;
+import com.learningSpringBoot.E_Commerce.Spring.Boot.application.domain.dto.ProductDto;
 import com.learningSpringBoot.E_Commerce.Spring.Boot.application.domain.entities.CategoryEntity;
+import com.learningSpringBoot.E_Commerce.Spring.Boot.application.domain.entities.ProductEntity;
 import com.learningSpringBoot.E_Commerce.Spring.Boot.application.mappers.Mapper;
+import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@AllArgsConstructor
 @Component
 public class CategoryMapperImpl implements Mapper<CategoryEntity, CategoryDto> {
 
     private final ModelMapper modelMapper;
+    private final Mapper<ProductEntity, ProductDto> productMapper;
 
-    public CategoryMapperImpl(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
-    }
 
     @Override
     public CategoryDto mapTo(CategoryEntity categoryEntity) {
-        return modelMapper.map(categoryEntity, CategoryDto.class);
+        CategoryDto dto = modelMapper.map(categoryEntity, CategoryDto.class);
+
+        if (categoryEntity.getProductEntities() != null) {
+            List<ProductDto> products = categoryEntity.getProductEntities()
+                    .stream()
+                    .map(productMapper::mapTo)   // uses ProductMapperImpl
+                    .toList();
+
+            dto.setProducts(products);
+        }
+        return dto;
     }
 
     @Override
